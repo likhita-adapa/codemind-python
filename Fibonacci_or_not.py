a2810f9a3cb0912647a034f10c5ada53c4b045eb
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-05-05 19:09:46 +0530

    n=int(input()) n1=0 n2=1 n3=0 j=0 for i in range(1,1+n,1):     n1=n2     n2=n3     n3=n1+n2     if(n3==n):         j=j+1 if j==0:     print("False") else:     print("True")

---
+++ Fibonacci_or_not.py
@@ -0,0 +1,15 @@
+n=int(input())
+n1=0
+n2=1
+n3=0
+j=0
+for i in range(1,1+n,1):
+    n1=n2
+    n2=n3
+    n3=n1+n2
+    if(n3==n):
+        j=j+1
+if j==0:
+    print("False")
+else:
+    print("True") 
