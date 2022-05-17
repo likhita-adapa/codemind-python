462bb47058b7eda39e46cc6af811e4e306a604f4
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-05-17 21:05:02 +0530

    n=int(input()) temp=n c=0 while(n>0):    n=n//10    c=c+1 n=temp*temp n=n%(pow(10,c)) if(temp==n):     print('Automorphic Number') else:     print('Not an Automorphic Number')

---
+++ Automorphic_Number.py
@@ -0,0 +1,12 @@
+n=int(input())
+temp=n
+c=0
+while(n>0):
+   n=n//10
+   c=c+1
+n=temp*temp
+n=n%(pow(10,c))
+if(temp==n):
+    print('Automorphic Number')
+else:
+    print('Not an Automorphic Number')
