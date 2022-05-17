1d4e057fd7f7fc4c9d922052c7348878f510a502
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-05-17 21:13:55 +0530

    n1=int(input()) n2=int(input()) a=0 for i in range(1,(n1//2)+1,1):     if(n1%i==0):         a=a+i b=0 for i in range(1,(n2//2)+1,1):     if(n2%i==0):         b=b+i if(a==n2 and b==n1):     print("Amicable") else:     print("Not Amicable")

---
+++ Amicable_Numbers.py
@@ -0,0 +1,15 @@
+n1=int(input())
+n2=int(input())
+a=0
+for i in range(1,(n1//2)+1,1):
+    if(n1%i==0):
+        a=a+i
+b=0
+for i in range(1,(n2//2)+1,1):
+    if(n2%i==0):
+        b=b+i
+if(a==n2 and b==n1):
+    print("Amicable")
+else:
+    print("Not Amicable")
+    
