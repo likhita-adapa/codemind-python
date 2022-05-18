4c0e50c5e4c5bd737f2b2c76cb72004ddcfbff3e
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-05-18 21:04:32 +0530

    num=int(input()) sum=0 temp=num while(num):     i=1     fact=1     rem=num%10     while(i<=rem):         fact=fact*i         i=i+1     sum=sum+fact     num=num//10 if(sum==temp):     print("The number",temp,"is a strong number") else:     print("The number",temp,"is not a strong number")

---
+++ Strong_Number.py
@@ -0,0 +1,16 @@
+num=int(input())
+sum=0
+temp=num
+while(num):
+    i=1
+    fact=1
+    rem=num%10
+    while(i<=rem):
+        fact=fact*i
+        i=i+1
+    sum=sum+fact
+    num=num//10
+if(sum==temp):
+    print("The number",temp,"is a strong number")
+else:
+    print("The number",temp,"is not a strong number")
