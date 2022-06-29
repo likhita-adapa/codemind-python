b81084dca3bc2b5aed29a3a054ccd54afcfda20b
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-29 19:07:55 +0530

    n=int(input()) k=-1 while(n):     k=-1     a,b=map(int,input().split())     for i in range(0,b+1):         if((i*i)%b==a):             k=i             break     print(k)     n=n-1

---
+++ Monk_and_Square_Root.py
@@ -0,0 +1,11 @@
+n=int(input())
+k=-1
+while(n):
+    k=-1
+    a,b=map(int,input().split())
+    for i in range(0,b+1):
+        if((i*i)%b==a):
+            k=i
+            break
+    print(k)   
+    n=n-1
