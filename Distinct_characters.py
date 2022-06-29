6097708077cb2b8123ec2df4fd961e3b7bde1d04
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-30 04:50:22 +0530

    n = input() n = n.lower() n = n.replace(" ","") s = [] for i in range(0,len(n)):     c = n.count(n[i])     if c==1:         s.append(n[i]) s.sort() s = str(s) s = s.replace(",","") s = s.replace("[","") s = s.replace("]","") s = s.replace(",","") s = s.replace("'","") s = s.replace(" ","") print(s)

---
+++ Distinct_characters.py
@@ -0,0 +1,18 @@
+n = input()
+n = n.lower()
+n = n.replace(" ","")
+s = []
+for i in range(0,len(n)):
+    c = n.count(n[i])
+    if c==1:
+        s.append(n[i])
+s.sort()
+s = str(s)
+s = s.replace(",","")
+s = s.replace("[","")
+s = s.replace("]","")
+s = s.replace(",","")
+s = s.replace("'","")
+s = s.replace(" ","")
+print(s)
+ 
