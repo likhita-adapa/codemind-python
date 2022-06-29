2a4f096757fe7ed03bbec28e64397f1780592871
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-29 23:02:20 +0530

    n=int(input()) a=list(map(int,input().split())) l=[] if n%2!=0:     for i in range(n//2+1):         if i!=(n-1)-i:             print(a[i],end=' ')             print(a[(n-1)-i],end=' ')     print(a[i],end=' ')     print('0') elif n%2==0:     for i in range(n//2):         if i!=(n-1)-i:             print(a[i],end=' ')             print(a[(n-1)-i],end=' ')

---
+++ First_and_Last_pair.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+l=[]
+if n%2!=0:
+    for i in range(n//2+1):
+        if i!=(n-1)-i:
+            print(a[i],end=' ')
+            print(a[(n-1)-i],end=' ')
+    print(a[i],end=' ')
+    print('0')
+elif n%2==0:
+    for i in range(n//2):
+        if i!=(n-1)-i:
+            print(a[i],end=' ')
+            print(a[(n-1)-i],end=' ')
