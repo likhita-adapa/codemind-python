79ef27c3d4e9fb4080004de014ac06e4758e04a7
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-30 05:02:39 +0530

    def anagram(s):     a=[]     for ch in s.lower():         a.append(ch)     d= {}     for ch in a:         if ch not in d:             d[ch] = 1         else:             d[ch] = d[ch] + 1     return d s1=input() s2=input() a = anagram(s1) b = anagram(s2) if a == b:     print("True") else:     print("False")

---
+++ Anagrams.py
@@ -0,0 +1,20 @@
+def anagram(s):
+    a=[]
+    for ch in s.lower():
+        a.append(ch)
+    d= {}
+    for ch in a:
+        if ch not in d:
+            d[ch] = 1
+        else:
+            d[ch] = d[ch] + 1
+    return d
+s1=input()
+s2=input()
+a = anagram(s1)
+b = anagram(s2)
+if a == b:
+    print("True")
+else:
+    print("False")
+ 
