59c5ebfff7c88004660af6181075a67a2ec3d5f9
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-29 19:16:47 +0530

    def is_palin(j):     m = j     s=0     while m!=0:         v = m%10         s = s*10+v         m = m//10     if j==s:         return j
    x = int(input())
    for i in range(x-1,1,-1):
        if is_palin(i):
            a = i
            break
    g = x+1
    while g!=0:
        if is_palin(g):
            b = g
            break
        g+=1
    if (x-a)<(b-x):
        print(a)
    elif (x-a)==(b-x):
        print(a,b)
    else:
        print(b)

---
+++ Closest_Palindrome_.py
@@ -0,0 +1,27 @@
+def is_palin(j):
+    m = j
+    s=0
+    while m!=0:
+        v = m%10
+        s = s*10+v
+        m = m//10
+    if j==s:
+        return j
+
+x = int(input())
+for i in range(x-1,1,-1):
+    if is_palin(i):
+        a = i
+        break
+g = x+1
+while g!=0:
+    if is_palin(g):
+        b = g
+        break
+    g+=1
+if (x-a)<(b-x):
+    print(a)
+elif (x-a)==(b-x):
+    print(a,b)
+else:
+    print(b)
