b4aed805b4e7e1e3bed84cead85663645c7cb913
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-29 20:49:50 +0530

    def rearrange(arr, n):     temp = n*[None]     small, large = 0, n-1     flag = True     for i in range(n):         if flag is True:             temp[i] = arr[large]             large -= 1         else:             temp[i] = arr[small]             small += 1         flag = bool(1-flag)     for i in range(n):         arr[i] = temp[i]     print(*arr) x=int(input()) l=[] for i in range(x):     n=int(input())     a=list(map(int,input().split()))     rearrange(a,n)

---
+++ Rearrange_Array_Alternately.py
@@ -0,0 +1,21 @@
+def rearrange(arr, n):
+    temp = n*[None]
+    small, large = 0, n-1
+    flag = True
+    for i in range(n):
+        if flag is True:
+            temp[i] = arr[large]
+            large -= 1
+        else:
+            temp[i] = arr[small]
+            small += 1
+        flag = bool(1-flag)
+    for i in range(n):
+        arr[i] = temp[i]
+    print(*arr)
+x=int(input())
+l=[]
+for i in range(x):
+    n=int(input())
+    a=list(map(int,input().split()))
+    rearrange(a,n)
