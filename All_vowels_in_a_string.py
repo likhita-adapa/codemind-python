bafb20f3195517656bf0ea37965c6bcf0bf7c181
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-30 04:18:21 +0530

    s=list(map(str,input().split())) b=[] c=[] for i in s:     for j in i:         if j=='a':             b.append(j)         elif j=='e':             b.append(j)         elif j=='i':             b.append(j)         elif j=='o':             b.append(j)         elif j=='u':             b.append(j)         elif j=='A':             c.append(j)         elif j=='E':             c.append(j)         elif j=='I':             c.append(j)         elif j=='O':             c.append(j)         elif j=='U':             c.append(j) n=len(set(b)) m=len(set(c)) if n==5:     print('True') elif m==5:     print('True') else:     print('False')

---
+++ All_vowels_in_a_string.py
@@ -0,0 +1,33 @@
+s=list(map(str,input().split()))
+b=[]
+c=[]
+for i in s:
+    for j in i:
+        if j=='a':
+            b.append(j)
+        elif j=='e':
+            b.append(j)
+        elif j=='i':
+            b.append(j)
+        elif j=='o':
+            b.append(j)
+        elif j=='u':
+            b.append(j)
+        elif j=='A':
+            c.append(j)
+        elif j=='E':
+            c.append(j)
+        elif j=='I':
+            c.append(j)
+        elif j=='O':
+            c.append(j)
+        elif j=='U':
+            c.append(j)
+n=len(set(b))
+m=len(set(c))
+if n==5:
+    print('True')
+elif m==5:
+    print('True')
+else:
+    print('False')
