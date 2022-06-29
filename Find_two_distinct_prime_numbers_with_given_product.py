997836978a041db9b0e60ca0cd54d391c9fc4c24
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-29 07:33:54 +0530

    def prime(n):     c=0     for i in range(2,n):         if n%i==0:             return False     if c==0:         return True n=int(input()) x=0 for i in range(1,n):     if prime(i):         for j in range(1,n):             if prime(j):                 if i*j==n:                     x=1                     print(i,end=' ')                     print(j,end=' ')     if x==1:         break
    if x==0:
        print("-1")

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,17 +1,23 @@
+def prime(n):
+    c=0
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    if c==0:
+        return True
 n=int(input())
-l=[]
-for i in range(2,n):
-    cnt=0
-    for j in range(2,i+1):
-        if(i%j==0):
-            cnt=cnt+1
-    if cnt<3:
-        l.append(i)
-cnt=0
-for k in range(len(l)):
-    for r in range(k+1,len(l)):
-        if l[k]*l[r]==n:
-            cnt=cnt+1
-            print(l[k],l[r])
-if cnt<1:
+x=0
+for i in range(1,n):
+    if prime(i):
+        for j in range(1,n):
+            if prime(j):
+                if i*j==n:
+                    x=1
+                    print(i,end=' ')
+                    print(j,end=' ')
+    if x==1:
+        break
+
+if x==0:
     print("-1")
+        
