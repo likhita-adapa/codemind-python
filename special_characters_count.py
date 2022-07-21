58205cd51a1a9ff49348c843089733a58bcd6b97
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-07-21 15:13:27 +0530

    x = input() c=0 for i in x:     if i.isdigit():         pass     elif i.islower():         pass     elif i.isupper():         pass     elif i==' ':         pass     else:         c+=1 print(c)

---
+++ special_characters_count.py
@@ -0,0 +1,14 @@
+x = input()
+c=0
+for i in x:
+    if i.isdigit():
+        pass
+    elif i.islower():
+        pass
+    elif i.isupper():
+        pass
+    elif i==' ':
+        pass
+    else:
+        c+=1
+print(c)
