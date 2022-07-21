0f0b0dbc03058f6fc44165b1d8e2d35af5e2f9d8
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-07-21 14:49:23 +0530

    a = list(map(str,input().lower().split())) b = list(map(str,input().lower().split())) s=0 l=[] m=[] for i in a:     if a.count(i)==1:         l.append(i) for i in b:     if b.count(i)==1:         m.append(i) for i in l:     if i in m:         s+=1 print(s)

---
+++ Common_words_-2.py
@@ -0,0 +1,15 @@
+a = list(map(str,input().lower().split()))
+b = list(map(str,input().lower().split()))
+s=0
+l=[]
+m=[]
+for i in a:
+    if a.count(i)==1:
+        l.append(i)
+for i in b:
+    if b.count(i)==1:
+        m.append(i)
+for i in l:
+    if i in m:
+        s+=1
+print(s)
