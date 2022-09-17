7d66fedf5602a2ab9a49db5985af6e3fa9ca59a1
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-09-17 20:59:15 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,(n//2)+1):         if n%i==0:             return 0     return 1 x = int(input()) l = list(map(int,input().split())) b=[] for i in l:     if prime(i):         b.append(i) print(format(sum(b)/len(b),".2f"))

---
+++ Average_of_primes.py
@@ -0,0 +1,14 @@
+def prime(n):
+    if n==1:
+        return 0
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return 0
+    return 1
+x = int(input())
+l = list(map(int,input().split()))
+b=[]
+for i in l:
+    if prime(i):
+        b.append(i)
+print(format(sum(b)/len(b),".2f"))
