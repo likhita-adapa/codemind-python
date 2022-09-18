f8ef7edcc7188082b7ae6eba8af03f8d22e6d7a8
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-09-18 15:01:26 +0530

    x=int(input()) l=list(map(int,input().split())) c=0 n=sum(l)//len(l) for i in l:     if i<=n:         c+=1 print(c)

--- elements_smaller_than_average.py
+++ elements_smaller_than_average.py
@@ -1,11 +1,8 @@
-n=int(input())
-a=list(map(int,input().split()))
-s=0
+x=int(input())
+l=list(map(int,input().split()))
 c=0
-for i in a:
-    s=s+i
-avg=s//n
-for j in a:
-    if j<=avg:
+n=sum(l)//len(l)
+for i in l:
+    if i<=n:
         c+=1
 print(c)
