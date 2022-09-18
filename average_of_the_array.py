8b75e20ce7cf047b869b256b649d84ef20530fcc
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-09-18 15:24:31 +0530

    x=int(input()) l=list(map(int,input().split())) print('%.2f'%(sum(l)/len(l)))

--- average_of_the_array.py
+++ average_of_the_array.py
@@ -1,7 +1,3 @@
-n=int(input())
-a=list(map(int,input().split()))
-s=0
-for i in range(n):
-    s=s+a[i]
-avg=s/n
-print('%.2f'%avg)
+x=int(input())
+l=list(map(int,input().split()))
+print('%.2f'%(sum(l)/len(l)))
