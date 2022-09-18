1c90bf2cd0c286dea33d174d04e981bba23e3e97
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-09-18 14:24:23 +0530

    x = int(input()) l = list(map(int,input().split())) a=[] b=[] for i in range(x):     if l[i]%2==0:         a.append(l[i])     else:         b.append(l[i]) j=0 i=0 while i<len(a) or j<len(b):     if j<len(b):         print(b[j],end=" ")         j+=1     if i<len(a):         print(a[i],end=" ")         i+=1 if x%2==1:     print('0')

--- odd_even_pairs.py
+++ odd_even_pairs.py
@@ -1,20 +1,20 @@
-n=int(input())
-a=list(map(int,input().split()))
-l=[]
-k=[]
-for i in range(n):
-    if a[i]%2==0:
-        l.append(a[i])
+x = int(input())
+l = list(map(int,input().split()))
+a=[]
+b=[]
+for i in range(x):
+    if l[i]%2==0:
+        a.append(l[i])
     else:
-        k.append(a[i])
-i=0
+        b.append(l[i])
 j=0
-while i<len(l) or j<len(k):
-    if j<len(k):
-        print(k[j],end=' ')
+i=0
+while i<len(a) or j<len(b):
+    if j<len(b):
+        print(b[j],end=" ")
         j+=1
-    if i<len(l):
-        print(l[i],end=' ')
+    if i<len(a):
+        print(a[i],end=" ")
         i+=1
-if n%2!=0:
+if x%2==1:
     print('0')
