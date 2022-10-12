3c962da604a2b8ef599dfb050e17d0438f6c0ff9
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-10-12 14:41:02 +0530

    s1=input().lower() s2=input().lower() s1=s1.split(' ') s2=s2.split(' ') for i in s2:     for j in s1:         if i==j:             print(i,end=' ')

--- common_words.py
+++ common_words.py
@@ -1,12 +1,8 @@
-x=list(map(str,input().lower().split()))
-y=list(map(str,input().lower().split()))
-l,m=[],[]
-for i in x:
-    if i in y:
-        l.append(i)
-for i in y:
-    if i in x:
-        m.append(i)
-for i in m:
-    if i in l:
-        print(i,end=" ")
+s1=input().lower()
+s2=input().lower()
+s1=s1.split(' ')
+s2=s2.split(' ')
+for i in s2:
+    for j in s1:
+        if i==j:
+            print(i,end=' ')
