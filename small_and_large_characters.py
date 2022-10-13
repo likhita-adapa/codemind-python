10ed80c1a3431638850033c5cc5b149b86f3c08e
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-10-13 20:06:42 +0530

    x = list(map(str,input().split())) s,m=0,0 for i in x:     print(min(i),end=" ")     print(max(i),end=" ")

--- small_and_large_characters.py
+++ small_and_large_characters.py
@@ -1,6 +1,5 @@
-s=input()
-x=s.split(" ")
+x = list(map(str,input().split()))
+s,m=0,0
 for i in x:
-    mi=min(i)
-    ma=max(i)
-    print(mi,ma,end=' ')
+    print(min(i),end=" ")
+    print(max(i),end=" ")
