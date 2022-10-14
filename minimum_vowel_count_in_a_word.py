d67d73f9bc33b107e91cbabe0cdd861456a0830d
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-10-14 12:02:58 +0530

    x = list(map(str,input().split())) c=0 l=[] for i in x:     for j in i:         if j=='a' or j=='e' or j=='i' or j=='o' or j=='u':             c+=1         elif j=='A' or j=='E' or j=='I' or j=='O' or j=='U':             c+=1     l.append(c)     c=0 print(min(l))

--- minimum_vowel_count_in_a_word.py
+++ minimum_vowel_count_in_a_word.py
@@ -1,7 +1,12 @@
-n=input()
-z=n.split()
+x = list(map(str,input().split()))
+c=0
 l=[]
-for i in z:
-    vowel=sum(letter in'aeiou' for letter in i.lower())
-    l.append(vowel)
+for i in x:
+    for j in i:
+        if j=='a' or j=='e' or j=='i' or j=='o' or j=='u':
+            c+=1
+        elif j=='A' or j=='E' or j=='I' or j=='O' or j=='U':
+            c+=1
+    l.append(c)
+    c=0
 print(min(l))
