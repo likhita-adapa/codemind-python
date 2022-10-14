e326c66e663d549e3c31e0347c9971cfba495d46
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-10-14 12:48:57 +0530

    n=int(input()) arr=list(map(int,input().split())) even=0 odd=0 for i in range(len(arr)):     if arr[i]%2==0:         even+=arr[i]     else:         odd+=arr[i] diff=abs(even-odd) print(diff)

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int,input().split()))
+even=0
+odd=0
+for i in range(len(arr)):
+    if arr[i]%2==0:
+        even+=arr[i]
+    else:
+        odd+=arr[i]
+diff=abs(even-odd)
+print(diff)
